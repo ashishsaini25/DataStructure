0d030aab25f34d61b59c44761bcc17ee86556775
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-14 06:57:26 +0530

    [Ashish] Add:stack UC1 Solved

---
+++ StackQueue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ StackQueue/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    public  class Stack
+    {
+        public Node head;
+        public Node Push(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+            {
+                head = newnode;
+                return head;
+            }
+            else
+            {
+                Node temp = head;
+                head = newnode;
+                newnode.next = temp;
+                return head;
+            }
+
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data + "->");
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine("null");
+        }
+
+    }
+}
